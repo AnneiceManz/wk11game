96a3d36e06f71453870f2dfe0bb46d6539abd438
Anneice Manzanares <mountains.stars@gmail.com>
2023-03-30 17:15:36 -0600

    added cards array and assets

---
+++ client/src/components/CardsArray.jsx
@@ -0,0 +1,35 @@
+import amethyst from '../assets/amethyst'
+import garnet from '../assets/garnet'
+import pearl from '../assets/pearl'
+import steven from '../assets/steven'
+import rose_quartz from '../assets/rose_quartz'
+import lion from '../assets/lion'
+
+const CardsArray = [
+    {
+        type: "Amethyst",
+        image: {amethyst}
+    },
+    {
+        type: "Garnet",
+        image: {garnet}
+    },
+    {
+        type: "Pearl",
+        image: {pearl}
+    },
+    {
+        type: "Steven",
+        image: {steven}
+    },
+    {
+        type: "Rose Quartz",
+        image: {rose_quartz}
+    },
+    {
+        type: "Lion",
+        image: {lion}
+    }
+  ]
+
+export default CardsArray
