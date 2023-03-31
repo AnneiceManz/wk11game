df6c78eb0bd40821704977588d6bbc25417950b8
Anneice Manzanares <mountains.stars@gmail.com>
2023-03-31 10:18:03 -0600

    update to server routes

---
+++ client/src/components/Scoreboard.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+const Scoreboard = () => {
+
+    const [scores, setScores] =useState([]);
+
+    
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Scoreboard;
--- server/server.js
+++ server/server.js
@@ -15,28 +15,26 @@ app.get('/', (req, res) => {
     res.json({ message: 'Hola, from My template ExpressJS with React-Vite' });
 });
 
-// create the get request for students in the endpoint '/api/students'
-app.get('/api/students', async (req, res) => {
+// create the get request for scores in the endpoint '/api/scoreboard'
+app.get('/api/scoreboard', async (req, res) => {
     try {
-        const { rows: students } = await db.query('SELECT * FROM students');
-        res.send(students);
+        const { rows: scoreboard } = await db.query('SELECT * FROM scoreboard');
+        res.send(scoreboard);
     } catch (e) {
         return res.status(400).json({ e });
     }
 });
 
 // create the POST request
-app.post('/api/students', async (req, res) => {
+app.post('/api/scoreboard', async (req, res) => {
     try {
-        const newStudent = {
-            firstname: req.body.firstname,
-            lastname: req.body.lastname,
-            iscurrent: req.body.iscurrent
+        const newScore = {
+            gamertag: req.body.gamertag,
+            score: req.body.score,
         };
-        //console.log([newStudent.firstname, newStudent.lastname, newStudent.iscurrent]);
         const result = await db.query(
-            'INSERT INTO students(firstname, lastname, is_current) VALUES($1, $2, $3) RETURNING *',
-            [newStudent.firstname, newStudent.lastname, newStudent.iscurrent],
+            'INSERT INTO scoreboard(gamertag, score) VALUES($1, $2) RETURNING *',
+            [newScore.gamertag, newScore.score],
         );
         console.log(result.rows[0]);
         res.json(result.rows[0]);
@@ -49,11 +47,11 @@ app.post('/api/students', async (req, res) => {
 });
 
 // delete request for students
-app.delete('/api/students/:studentId', async (req, res) => {
+app.delete('/api/students/:player_id', async (req, res) => {
     try {
-        const studentId = req.params.studentId;
-        await db.query('DELETE FROM students WHERE id=$1', [studentId]);
-        console.log("From the delete request-url", studentId);
+        const player_id = req.params.player_id;
+        await db.query('DELETE FROM scoreboard WHERE player_id=$1', [player_id]);
+        console.log("From the delete request-url", player_id);
         res.status(200).end();
     } catch (e) {
         console.log(e);
@@ -62,27 +60,6 @@ app.delete('/api/students/:studentId', async (req, res) => {
     }
 });
 
-//A put request - Update a student 
-app.put('/api/students/:studentId', async (req, res) =>{
-    //console.log(req.params);
-    //This will be the id that I want to find in the DB - the student to be updated
-    const studentId = req.params.studentId
-    const updatedStudent = { id: req.body.id, firstname: req.body.firstname, lastname: req.body.lastname, iscurrent: req.body.is_current}
-    console.log("In the server from the url - the student id", studentId);
-    console.log("In the server, from the react - the student to be edited", updatedStudent);
-    // UPDATE students SET lastname = "something" WHERE id="16";
-    const query = `UPDATE students SET firstname=$1, lastname=$2, is_current=$3 WHERE id=${studentId} RETURNING *`;
-    const values = [updatedStudent.firstname, updatedStudent.lastname, updatedStudent.iscurrent];
-    try {
-      const updated = await db.query(query, values);
-      console.log(updated.rows[0]);
-      res.send(updated.rows[0]);
-  
-    }catch(e){
-      console.log(e);
-      return res.status(400).json({e})
-    }
-  })
 
 // console.log that your server is up and running
 app.listen(PORT, () => {
